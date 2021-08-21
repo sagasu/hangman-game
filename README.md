5368c5c80942f97d88d7cca33070457c2eb0b671
Mateusz Kopij <sagasu@gmail.com>
2021-08-21 09:30:53 +0100

    guessing is now enabled

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # hangman-game
 
-# How to install elm w/o installing it globally (-g option).
+# How to create elm w/o installing it globally (-g option).
 run this in folder where this README.md is, it will add only in a scope of a given `cmd` session bin tools from node modules. No other application will be affected, the scope is a single `cmd` session.  
 
 `mkdir hangman-game`  
--- hangman/src/Main.elm
+++ hangman/src/Main.elm
@@ -3,18 +3,22 @@ module Main exposing (..)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (src)
+import Html.Events exposing (..)
+import Set exposing (Set)
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {}
+    {guesses: Set String}
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( {
+        guesses = Set.empty
+    }, Cmd.none )
 
 phrase: String
 phrase = "hangman in elm"
@@ -23,12 +27,14 @@ phrase = "hangman in elm"
 
 
 type Msg
-    = NoOp
+    = Guess String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        Guess char -> 
+            ( {model | guesses = Set.insert char model.guesses }, Cmd.none)
 
 
 
@@ -44,6 +50,8 @@ view model =
             |> List.map(\char -> 
                 if char == " " then
                     " "
+                else if Set.member char model.guesses then
+                    char
                 else
                     "_"
                 )
@@ -52,17 +60,31 @@ view model =
                     span [] [text char]
                 )
             |> div []
+
+        phraseSet = 
+            phrase
+            |> String.split ""
+            |> Set.fromList
+
+        failuresHtml = 
+            model.guesses
+            |> Set.toList
+            |> List.filter (\char -> not <| Set.member char phraseSet)
+            |> List.map (\char -> span [] [text char])
+            |> div []
+
         buttonsHtml = "abcdefghijklmnopqrstuvwxyz"
                       |> String.split ""
                       |> List.map (\char ->
-                        button[] [text char]
+                        button[onClick <| Guess char] [text char]
                        )
                       |> div []
     in
     div []
         [
             phraseHtml,
-            buttonsHtml
+            buttonsHtml,
+            failuresHtml
         ]
 
 
